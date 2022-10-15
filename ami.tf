9cb5ca2c34a0f516180d094fc3a8c7b6ee03df78
Raghu K <raghuk.vit@gmail.com>
2022-10-15 07:55:06 +0530

    Update app-code/shipping/

--- ami.tf
+++ ami.tf
@@ -1,7 +1,7 @@
 module "ami" {
   source      = "github.com/raghudevopsb66/terraform-immutable-ami"
   APP_VERSION = var.APP_VERSION
-  COMPONENT   = "cart"
+  COMPONENT   = "shipping"
 }
 
 variable "APP_VERSION" {}
@@ -9,7 +9,7 @@ variable "APP_VERSION" {}
 terraform {
   backend "s3" {
     bucket = "terraform-b66"
-    key    = "ami/cart/terraform.tfstate"
+    key    = "ami/shipping/terraform.tfstate"
     region = "us-east-1"
   }
 }
